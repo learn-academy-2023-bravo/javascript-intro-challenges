048cd9ff5cc93ef6d481beac075d60ad0cdfd2b1
JAKEDAHLGREN <Bigbear0649@gmail.com>
2023-03-15 16:05:15 -0700

    First wave

---
+++ conditionals-go-jd.js
@@ -0,0 +1,42 @@
+// Write the code that will log true or false for the following:
+
+// Is 34 divided by 3 greater than 67 divided by 2?
+console.log(34 / 3 > 67 / 2) // False
+
+// Does 5 evaluate to the same as "5"?
+console.log(5 == "5") // True
+
+// Does 5 strictly equal "5"?
+console.log(5 === "5") // False
+
+// Does !3 strictly equal 3?
+console.log(!3 === 3) // False
+
+// Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
+console.log("LEARN".length === 5 && "Student".length === 7) // True
+
+// Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
+console.log("LEARN".length === 5 || "Student".length === 10) // True
+
+// Does "LEARN" contain the subset "RN"?
+
+
+// Does "LEARN" contain the subset "rn"?
+
+
+// Does "LEARN"[0] strictly equal "l"?
+
+
+// Modify the code from the previous question to return true.
+
+// Make sure you try different options and change the variables to ensure properly working code.
+
+// Write a statement that takes a variable of item and logs "in budget" if a price is $100 or less.
+
+// Write a statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
+
+// Write a statement that takes a variable of trafficLight and logs "go" if the light is green, "slow down" if the light is yellow and "stop" if the light is red.
+
+// Write a statement that takes two variables that are numbers and outputs the larger number. If the numbers are equal, output "the numbers are the same".
+
+// Write a statement that takes a variable of a number and logs whether the number is odd, even, or zero.
