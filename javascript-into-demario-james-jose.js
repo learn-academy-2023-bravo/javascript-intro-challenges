6fcd5c1a7c278539b907dbae89c0ed64a29be18d
jameshlee1 <james.h.lee3@outlook.com>
2023-03-15 15:27:21 -0700

    Chanllenges accepted

--- javascript-into-demario-james-jose.js
+++ javascript-into-demario-james-jose.js
@@ -39,3 +39,54 @@ console.log (yourName.indexOf("J")) // 0
 // Write the code that will log the string in all uppercase letters.
 
 console.log (yourName.toUpperCase()) // JOSE
+
+const theQuestion = "life, the universe, and everything"
+const theAnswer = 42
+
+// Write the code that will log theAnswer divided by 2.
+
+console.log (theAnswer/2)// 21
+
+// Write the code that will log the whole number remainder of theAnswer when divided by 3.
+
+console.log (theAnswer%3)// 0
+
+// Write the code that will log the length of theQuestion.
+
+console.log (theQuestion.length) //34
+
+// Write the code that will log the length of theQuestion divided by 2.
+
+console.log (34/2)// 17
+
+// Write the code that will log the length of theQuestion added to theAnswer.
+
+console.log (34+42)// 76
+
+// Write the code that will log the index of the character "f" in the theQuestion.
+
+console.log (theQuestion.indexOf("f"))// 2
+
+// Write the code that will log the index of the second comma in the theQuestion.
+
+console.log (theQuestion.indexOf(",", theQuestion.indexOf (",") +1))// 18
+
+// Write the code that will log the concatenation of the two variables.
+
+console.log (theQuestion.concat(" ", theAnswer))// life, the universe, and everything 42
+
+
+// Write the code that will log the character "l" from theQuestion.
+
+console.log (theQuestion.charAt(0))// l
+
+// Write the code that will extract "the universe" from theQuestion.
+
+console. log (theQuestion.substring("the universe"))
+
+
+// Write the code that will extract "everything" from theQuestion.
+
+
+
+// Write the code that will log the last character of theQuestion without manually counting the number of characters.
