c52c7acce8e86d7ee3c97e1717b88482107f29be
Valeria <valeriacasillas123@yahoo.com>
2023-03-15 16:16:46 -0700

    first half done

---
+++ conditionals-valeria-wagner.js
@@ -0,0 +1,29 @@
+//Is 34 divided by 3 greater than 67 divided by 2?
+ //console.log(34/3>67/2)//false
+
+// Does 5 evaluate to the same as "5"?
+//console.log(5=="5")//True
+
+// Does 5 strictly equal "5"?
+//console.log(5==="5")//False
+
+// Does !3 strictly equal 3?
+//console.log(!3===3) False
+
+// Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
+//console.log("Learn".length ===5)True
+//console.log("Student".length ===7)True
+
+// Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
+//console.log("Learn".length ===5 || "Student".length ===10) True
+// Does "LEARN" contain the subset "RN"?
+//console.log("LEARN".includes ("RN")) True
+
+// Does "LEARN" contain the subset "rn"?
+//console.log("LEARN".includes ("rn"))  FALSE
+
+// Does "LEARN"[0] strictly equal "l"?
+//console.log("LEARN".charAt (0)==="l") FALSE
+
+// Modify the code from the previous question to return true.
+//console.log("LEARN"[0]==="L")True
