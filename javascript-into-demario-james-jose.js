890c6693ee11cf2ef213ab3bad2a8cd060737891
salas.89jl <salas.89jl@gmail.com>
2023-03-15 11:46:19 -0700

    Completed first part of javascript challenge

---
+++ javascript-into-demario-james-jose.js
@@ -0,0 +1,41 @@
+// Write the code that will log the outcome of 34 added to 71.
+
+console.log(34 + 71) // 105
+
+// Write the code that will log the outcome of 67 subtracted from 123.
+
+console.log(123 - 67) // 56
+
+// Write the code that will log the outcome of 56 multiplied by 23.
+
+console.log (56 * 23) // 1288
+
+// Write the code that will log the outcome of 45 divided by 5.
+
+console.log (45 / 5) // 9
+
+// Write the code that will log the outcome of 5 to the power of 7.
+
+console.log (5 ** 7) // 78125
+
+// Write the code that will log the whole number remainder of 33 divided by 6.
+
+console.log (33 % 6) // 3 
+
+// Write the code that will log the length of a string containing your name.
+
+var yourName = "Jose"
+
+console.log (yourName.length) // 4
+
+// Write the code that will log whether your string includes the letter "e"?
+
+console.log (yourName.includes("e")) // true
+
+// Write the code that will log the character at the first index of the string.
+
+console.log (yourName.indexOf("J")) // 0
+
+// Write the code that will log the string in all uppercase letters.
+
+console.log (yourName.toUpperCase()) // JOSE
