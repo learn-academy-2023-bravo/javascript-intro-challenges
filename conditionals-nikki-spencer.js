4e5fbb3f72bebbe7e5a7ee8c787b9a0d2c71bc36
LEARN Academy <sdunlap102326@gmail.com>
2023-03-15 16:29:57 -0700

    Answers To Questions

---
+++ conditionals-nikki-spencer.js
@@ -0,0 +1,97 @@
+if(34 / 3 > 67 / 2) {
+    console.log(true)
+}
+else {
+console.log(false)
+}
+
+if(5 == "5") {
+    console.log(true)
+}
+else {
+console.log(false)
+}
+
+if(5 === "5") {
+    console.log(true)
+}
+else {
+console.log(false)
+}
+
+if(!3 == "3") {
+    console.log(true)
+}
+else {
+console.log(false)
+}
+
+if(5 == "5") {
+    console.log(true)
+}
+else {
+console.log(false)
+}
+
+if("LEARN".length === 5 && "Student".length === 7){
+    console.log(true)
+}
+else {
+    console.log(false)
+}
+
+if("LEARN".length === 5 && "Student".length === 10){
+    console.log(true)
+}
+else {
+    console.log(false)
+}
+
+if("LEARN".includes("RN")) {
+    console.log(true)
+}
+else {
+    console.log(false)
+}
+
+if("LEARN".includes("rn")) {
+    console.log(true)
+}
+else {
+    console.log(false)
+}
+
+if("LEARN"[0] === ("l")) {
+    console.log(true)
+}
+else {
+    console.log(false)
+}
+
+if("LEARN"[0] === ("L")) {
+    console.log(true)
+}
+else {
+    console.log(false)
+}
+
+var price = prompt("Please enter price:")
+if(price <= 100) {
+    console.log("in budget")
+}
+else {
+    alert("You're broke!")
+}
+
+var trafficLight = prompt("What color is the traffic light?")
+if(trafficLight === "yellow") {
+alert("Slow down!")
+}
+ else if(trafficLight === "red"){
+alert("Slam on brakes!")
+}
+else {
+    alert("Goooo!")
+}
+
+
