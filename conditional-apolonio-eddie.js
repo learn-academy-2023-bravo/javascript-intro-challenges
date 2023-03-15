c7ca24fcce5cc629023a5582b0c848c5cd2e55af
SunGodApollo <apple.ortiz@hotmail.com>
2023-03-15 16:06:35 -0700

    conditionals start

---
+++ conditional-apolonio-eddie.js
@@ -0,0 +1,18 @@
+// Write the code that will log true or false for the following:
+// Is 34 divided by 3 greater than 67 divided by 2?
+// Does 5 evaluate to the same as "5"?
+// Does 5 strictly equal "5"?
+// Does !3 strictly equal 3?
+// Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
+// Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
+// Does "LEARN" contain the subset "RN"?
+// Does "LEARN" contain the subset "rn"?
+// Does "LEARN"[0] strictly equal "l"?
+// Modify the code from the previous question to return true.
+
+console.log(34/3 > 67/2)
+console.log(5 === "5")
+console.log(!3 === 3)
+console.log("LEARN".length === 5 || "Student".length === 10)
+console.log("LEARN".includes("RN"))
+
