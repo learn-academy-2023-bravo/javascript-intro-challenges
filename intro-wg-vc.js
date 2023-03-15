9913d7faf5d582030f4da3bdbe39f6a7f9cdb428
wagnergon <wagnergon52@gmail.com>
2023-03-15 11:50:28 -0700

    first half problems done

---
+++ intro-wg-vc.js
@@ -0,0 +1,24 @@
+// Write the code that will log the outcome of 34 added to 71.
+console.log(34+71)
+// Write the code that will log the outcome of 67 subtracted from 123.
+console.log(123-67)
+// Write the code that will log the outcome of 56 multiplied by 23.
+console.log(56*23)
+// Write the code that will log the outcome of 45 divided by 5.
+console.log(45/5)
+// Write the code that will log the outcome of 5 to the power of 7.
+console.log(5**7)
+// Write the code that will log the whole number remainder of 33 divided by 6.
+console.log(33%6)
+// Write the code that will log the length of a string containing your name.
+var myName = "Wagner"
+console.log(myName.length) 
+
+console.log("wagner".length)
+// Write the code that will log whether your string includes the letter "e"?
+console.log(myName.includes("e"))
+console.log("wagner".includes("e"))
+// Write the code that will log the character at the first index of the string.
+console.log(myName.charAt(0))
+// Write the code that will log the string in all uppercase letters.
+console.log(myName.toUpperCase())
