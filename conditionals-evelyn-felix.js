a544d4b74d03795cd5fab8236715dee139e2c2b5
Ealvarez01 <e.alvarez2404@yahoo.com>
2023-03-15 15:59:22 -0700

    killing this java code

---
+++ conditionals-evelyn-felix.js
@@ -0,0 +1,25 @@
+let x = 34
+let y = 67
+
+console.log (x / 3 > y / 2)
+//output: false
+console.log (5 == "5")
+//output: true
+console.log (5 === "5") 
+//output: false
+console.log (!3 === 3)
+//output: false
+console.log ("learn".length === 5 && "student".length === 7)
+//output: true
+console.log ("learn".length === 5 || "student".length === 10)
+//output: true
+
+let word = "Learn"
+console.log (word.includes ("rn"))
+//output: true
+console.log (word.includes ("RN"))
+//output: false
+console.log (word [0] === "l")
+//output: false
+console.log (word [0] === "L")
+//output: true
