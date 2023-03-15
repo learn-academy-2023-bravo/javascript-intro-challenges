22abf9ff0ddf13a2f8da4a7f58007e5e751891b8
Valeria <valeriacasillas123@yahoo.com>
2023-03-15 16:39:21 -0700

    Almost done

--- conditionals-valeria-wagner.js
+++ conditionals-valeria-wagner.js
@@ -27,3 +27,29 @@
 
 // Modify the code from the previous question to return true.
 //console.log("LEARN"[0]==="L")True
+
+//Write a statement that takes a variable of item and logs "in budget" if a price is $100 or less.
+var item = 80
+if (item <=100){
+    console.log("In budget")
+}
+// Write a statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
+var hungry=false
+if (hungry===true){
+    console.log("eat food")
+}else if (hungry===false){
+    console.log("keep coding")
+}
+// Write a statement that takes a variable of trafficLight and logs "go" if the light is green, "slow down" if the light is yellow and "stop" if the light is red.
+var trafficLight = "red"
+if (trafficLight === "green"){
+    console.log("go")
+}else if(trafficLight === "yellow"){
+    console.log("slow down")
+}else if(trafficLight ==="red"){
+    console.log("Stop")
+}
+
+// Write a statement that takes two variables that are numbers and outputs the larger number. If the numbers are equal, output "the numbers are the same".
+
+// Write a statement that takes a variable of a number and logs whether the number is odd, even, or zero
