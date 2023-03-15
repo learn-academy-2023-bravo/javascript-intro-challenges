a905a241c2ac5035c807689842a2a411c8ba90d4
FLopez73 <flopez730@gmail.com>
2023-03-15 11:40:17 -0700

    java practice code

---
+++ javascript-intro-evelyn-felix.js
@@ -0,0 +1,24 @@
+console.log (34 + 71)
+
+console.log (67 - 123)
+
+console.log (56 * 23)
+
+console.log (45 / 5)
+
+console.log (5 ** 7)
+
+console.log (33 % 6)
+
+var name = "Evelyn"
+
+console.log (name.length)
+
+console.log (name.includes ("e"))
+
+console.log (name[0])
+
+console.log (name.charAt(0))
+
+//Consider the variables:
+
