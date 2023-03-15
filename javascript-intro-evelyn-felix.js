7c83a11c9f905f4636412acaa7c71cd673321386
Ealvarez01 <e.alvarez2404@yahoo.com>
2023-03-15 15:29:12 -0700

    we got it

--- javascript-intro-evelyn-felix.js
+++ javascript-intro-evelyn-felix.js
@@ -22,3 +22,29 @@ console.log (name.charAt(0))
 
 //Consider the variables:
 
+const theQuestion = "life, the universe, and everything"
+const theAnswer = 42
+
+console.log (theAnswer / 2)
+
+console.log (theAnswer % 3)
+
+console.log (theQuestion.length)
+
+console.log (theQuestion.length / 2)
+
+console.log (theQuestion.length + theAnswer)
+
+console.log (theQuestion [2])
+
+console.log (theQuestion [18])
+
+console.log (theQuestion + theAnswer)
+
+console.log (theQuestion.charAt (0))
+
+console.log (theQuestion.slice (6, 18))
+
+console.log (theQuestion.slice (24, 35))
+
+console.log (theQuestion.charAt(theQuestion.length - 1))
