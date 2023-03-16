b48cd78d5a0fd2204c573fa3ce113dccccccde2c
Ealvarez01 <e.alvarez2404@yahoo.com>
2023-03-16 16:42:16 -0700

    arrays-dre-evelyn.js

--- arrays-dre-evelyn.js
+++ arrays-dre-evelyn.js
@@ -107,39 +107,5 @@ console.log(subsetArray)
 
 // Write the code that adds the values from odd indexes into the oddIndexes array.
 
-// let index = 0
-// let index <20
-// index = index + 1
 
-// let numsArray = [1, 2, 3, 4, 5, 6, 7 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-//   console.log(numsArray.length)
-
-// for (let i = 0; i <numsArray.length; i++) {
-//   console.log ("index:", i, "value:", numsArray [i])
-// }
-
-let numsArray = []
-
-//Create a for loop that logs each number from 1 - 20.
-for(let i=0; i<21; i++){
-    console.log(i)
-}
-
-//Create a for loop that logs every other number from 1 - 20.
-for(let i=0; i<21; i+=2) {
-    console.log(i)
-}
-
-//Create a for loop that logs the result of each number from 1 - 20 tripled.
-for(let i=0; i<21; i++) {
-    console.log(i * 3)
-}
-
-//Create a for loop that logs each even number from 1-20, and in the place of every odd number, returns the word "ODD".
-for(let i=0; i<21; i++) {
-    if ([i] % 2 === 1){
-        console.log("ODD")
-    } else if([i] % 2 === 0) {
-        console.log(i)
-    }
 
