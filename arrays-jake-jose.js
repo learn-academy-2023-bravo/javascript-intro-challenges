0182e0fb5545777450181c7ce9c7d9539a7f8b3e
salas.89jl <salas.89jl@gmail.com>
2023-03-16 12:57:35 -0700

    Seconding part of array challenges completed.

--- arrays-jake-jose.js
+++ arrays-jake-jose.js
@@ -1,6 +1,6 @@
 // Consider the variable:
 
-    var groceryList = ["chips", "dip", "cookies"]
+    // var groceryList = ["chips", "dip", "cookies"]
 
 // Write the code that will add "soda" to the end of the original array.
     // groceryList.push("soda")
@@ -11,8 +11,8 @@
     //  console.log(groceryList) ----?
 
 // Write the code that will return a subset of the array containing only "chips" and "dip".
-    groceryList.pop("cookies")
-    console.log(groceryList)
+    // groceryList.pop("cookies")
+    // console.log(groceryList)
 
 // Write the code that will add "beans" to the "chips" and "dip" array.
     // groceryList.push("beans")
@@ -21,14 +21,31 @@
 // Consider the variable:
 
 
-// var numbers = [2, 4, 6, 8, 10]
+var numbers = [2, 4, 6, 8, 10]
+
 // Write the code that will add the number 0 to the beginning of the array.
+
+// numbers.unshift(0)
+// console.log(numbers)
+// // --> [ 0, 2, 4, 6, 8, 10 ]
 
 // Write the code that will add the number 12 to the end of the array.
 
+numbers.push(12)
+console.log(numbers)
+
 // Write the code that will remove the first number from the array.
 
+console.log("remove", numbers.shift())
+console.log("remaining number:", numbers)
+
 // Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
+
+// console.log("0,".concat(numbers))
+// // --> 0,2,4,6,8,10
+
+// console.log([0].concat(numbers))
+// // --> [ 0, 2, 4, 6, 8, 10 ]
 
 // Consider the variable:
 
