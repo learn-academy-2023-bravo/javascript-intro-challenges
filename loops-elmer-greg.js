4c007e956ae8cbc727d8492b7f683429fb6eb2ec
gnostertag <ostertag2000@gmail.com>
2023-03-16 16:19:04 -0700

    loops

--- loops-elmer-greg.js
+++ loops-elmer-greg.js
@@ -3,14 +3,22 @@ for(let i=1;i<21;i++){
     console.log(i)
 }
 // Create a for loop that logs every other number from 1 - 20.
-for(let i=1;i<21;i++){
-    console.log(i%2!==0)
+for(let i=1;i<21;i+=2){
+    console.log(i)
 }
 // Create a for loop that logs the result of each number from 1 - 20 tripled.
 for(let i=1;i<21;i++){
     console.log(i*3)
 }
 // Create a for loop that logs each even number from 1-20, and in the place of every odd number, returns the word "ODD".
+for(let i=1; i<21; i++) {
+    if ([i] % 2 === 1){
+        console.log("ODD")
+    } else if([i] % 2 === 0) {
+        console.log(i)
+    }
+}
+
 // Expected output: ODD, 2, ODD, 4, ODD, 6 ...etc
 // Consider this variable:
 
