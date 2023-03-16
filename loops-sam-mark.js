e50ee5c27a44cb572d8552e1e9163f75bdc49025
MarkNou <mark.nou95@gmail.com>
2023-03-16 16:08:49 -0700

    All Completed

--- loops-sam-mark.js
+++ loops-sam-mark.js
@@ -65,10 +65,54 @@ for(let i=0; i<nums.length;i++){
 // Consider this variable:
 
 // const myString = "learn student"
+const myString = "learn student"
+
 // Create the code that will log the number of times the letter "e" occurs in the string.
+
+var eTotal = 0
+
+for(let i=0;i<myString.length;i++) {
+    if (myString[i] === "e") {
+        eTotal++
+    }
+}
+console.log(eTotal)
+
 // Create the code that will log every other character in the string.
+
+for(let i=0;i<myString.length;i++) {
+    if(myString[i] !== "e") {
+        console.log(myString[i])
+    }
+}
 // 🏔 Stretch Goals
 // Create the code that iterates from 5 to 15. For each iteration log if the current number is odd or even.
+
+for(let i=5;i<16;i++) {
+    if(i % 2 === 0) {
+        console.log("even")
+    } else(console.log("odd")) 
+    }
+
 // Expected output: "5 is odd" "6 is even" "7 is odd" ...etc
+
+for(let i=5;i<16;i++) {
+    if(i % 2 === 0) {
+        console.log(`${i} is even`)
+    } else(console.log(`${i} is odd`)) 
+    }
 // Fizz Buzz: Create the code that will iterate from 1-100. If a number is a multiple of 3, replace it with the word fizz. If a number is a multiple of five, replace it with the word buzz. If a number is a multiple of both 3 and 5, replace it with fizzbuzz.
+
+for(let i=1;i<101;i++) {
+    if(i % 3 === 0 && i % 5 === 0){ 
+        console.log("fizzbuzz")
+    }
+            else if(i % 3 === 0) { 
+                console.log("fizz")
+            } 
+                    else if(i % 5 === 0) { 
+                        console.log("buzz")
+                    } else (console.log(i))
+                }
+    
 // Expected output: 1, 2, "fizz", 4, "buzz", "fizz", 7, 8, "fizz", "buzz", 11, "fizz", 13, 14, "fizzbuzz" ...etc
