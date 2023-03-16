f7fe83ad8c09085c952025ceeee574cec95fd333
wagnergon <wagnergon52@gmail.com>
2023-03-16 12:01:38 -0700

    half problems done

---
+++ arrays-wagner-nikki.js
@@ -0,0 +1,35 @@
+var groceryList = ["chips", "dip", "cookies"]
+
+//Write the code that will add "soda" to the end of the original array.
+// console.log(groceryList.push("soda"))
+console.log(groceryList)
+console.log(groceryList.concat("soda"))
+
+//Write the code that will add "granola" to the end of array without altering the original array.
+console.log(groceryList.concat("granola"))
+
+//Write the code that will return a subset of the array containing only "chips" and "dip".
+console.log(groceryList.slice(0,2))
+
+//Write the code that will add "beans" to the "chips" and "dip" array.
+var slicedArray = groceryList.slice(0,2)
+console.log(slicedArray.concat("beans"))
+
+var numbers = [2, 4, 6, 8, 10]
+// Write the code that will add the number 0 to the beginning of the array.
+//console.log(numbers.unshift("0"))
+//console.log(numbers)
+
+// Write the code that will add the number 12 to the end of the array.
+//console.log(numbers.push("12"))
+//console.log(numbers)
+//console.log(numbers.concat("12"))
+
+// Write the code that will remove the first number from the array.
+//console.log(numbers.shift())
+//console.log(numbers)
+
+// Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
+
+
+
