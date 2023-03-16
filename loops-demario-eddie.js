51c28d13199900106e996c9a0d606bd7b23a918c
EddieCarrion.github.io <ecar25L@gmail.com>
2023-03-16 16:05:57 -0700

    challenges completed

---
+++ loops-demario-eddie.js
@@ -0,0 +1,63 @@
+// // Create a for loop that logs each number from 1 - 20.
+// for (i=1;i<21;i++){
+//     console.log(i)
+// }
+
+// Create a for loop that logs every other number from 1 - 20.
+// for (i=1;i<21;i++){
+//     if (i%2==0){
+//         console.log(i)
+//     }
+// }
+
+// Create a for loop that logs the result of each number from 1 - 20 tripled.
+// for (i=1;i<21;i++){
+//     console.log (i*3)
+// }
+
+// Create a for loop that logs each even number from 1-20, and in the place of every odd number, returns the word "ODD".
+// Expected output: ODD, 2, ODD, 4, ODD, 6 ...etc
+// for (i = 1; i < 21; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i)
+//     } 
+//     else console.log ("odd")
+// }
+const nums = [3, 57, -9, 20, 67]
+// Create the code that will log the largest number from the array.
+// let x=0
+// for (let i=0; i<nums.length;i++){
+//     if (nums[i]>x){
+//         x=nums[i]
+//     }
+// } console.log (x)
+// Create the code that will log the smallest number from the array.
+// letfor (let i=0; i<nums.length;i++){
+//     if (nums[i]<x){
+//         x=nums[i]
+//     } x= nums [0]
+// 
+// } console.log (x)
+// Create the code that will log the remainder of each number when divided by 2. Expected output: 1, 1, -1, 0, 1
+
+//for (let i=0; i<nums.length;i++){
+        //console.log(nums[i]%2)
+        //}
+// Consider this variable:
+
+const myString = "learn student"
+// Create the code that will log the number of times the letter "e" occurs in the string.
+
+// let e=0 
+// for (let i=0; i<myString.length; i++){
+//     if (myString[i]=="e"){
+//         e++
+
+//     } 
+// } console.log (e)
+// Create the code that will log every other character in the string.
+
+for (let i=0; i<myString.length; i++){
+    console.log (myString[i])
+    i++
+}
