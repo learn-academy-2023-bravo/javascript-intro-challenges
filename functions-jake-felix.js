ecea995887cf1c56042e1769323000daa4a31f05
JAKEDAHLGREN <Bigbear0649@gmail.com>
2023-03-17 11:58:00 -0700

    First section of challenge

---
+++ functions-jake-felix.js
@@ -0,0 +1,43 @@
+// Write a function named marco that returns "polo".
+    // make a variable named "marco" and run our function to return a string of "polo"
+
+    // const marco = ()=>{
+    //     return "polo"
+    // } 
+    // console.log(marco()) 
+
+// Write a function named greeting that takes a name as an argument and returns "Welcome, <person's name here>!"
+
+    // set a variable as greeting, then create a function,and then create a string interpolation to return "welcome," plus "person's name"
+    // const greeting = (name)=> {
+    //     return `Welcome, ${name}`    
+    // }
+    // console.log(greeting("jake"))
+    // console.log(greeting("felix"))
+
+// Write a function named oddOrEven that takes a number as an argument and returns whether the number is odd or even.
+
+    // create a function named oddOrEven, create an array with multiple numbers, set conditional state of if odd then return "odd", else if even then return "even", else as a catch all to return "you messed up"
+
+//  const oddOrEven = (number)=> {
+//     if(number % 2 === 0)
+//     return "even"
+//     else if(number % 2 !== 0)
+//     return "odd"
+//     }
+// console.log(oddOrEven(9))
+// console.log(oddOrEven(8))
+
+// Write a function named triple that takes a number as an argument and returns the result of that number multiplied by 3.
+
+// Write a function named multiply that takes two numbers as arguments and returns the result of the numbers multiplied together.
+
+// Write a function named divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
+
+// Write a function named assignGrade that takes a number score as an argument and returns the letter grade for the score.
+
+// Write a function named isLonger that takes two strings as arguments and returns the string that contains the most characters.
+
+// Write a function named greaterNum that takes two numbers as arguments and returns whichever number is the greater (higher) number.
+
+// -. Write a function named yelling that takes a string as an argument and return the string in all uppercase case letters.
