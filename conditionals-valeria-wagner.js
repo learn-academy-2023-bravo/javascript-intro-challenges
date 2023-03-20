fb8f4d08be4b3e95410360d97bcd63144b395632
Valeria <valeriacasillas123@yahoo.com>
2023-03-20 11:33:41 -0700

    Done

--- conditionals-valeria-wagner.js
+++ conditionals-valeria-wagner.js
@@ -1,32 +1,33 @@
 //Is 34 divided by 3 greater than 67 divided by 2?
- //console.log(34/3>67/2)//false
+ console.log(34/3>67/2)//false
 
 // Does 5 evaluate to the same as "5"?
-//console.log(5=="5")//True
+console.log(5=="5")//True
 
 // Does 5 strictly equal "5"?
-//console.log(5==="5")//False
+console.log(5==="5")//False
 
 // Does !3 strictly equal 3?
-//console.log(!3===3) False
+console.log(!3===3) //False
 
 // Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
-//console.log("Learn".length ===5)True
-//console.log("Student".length ===7)True
+console.log("Learn".length ===5)//True
+console.log("Student".length ===7)//True
 
 // Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
-//console.log("Learn".length ===5 || "Student".length ===10) True
+console.log("Learn".length ===5 || "Student".length ===10)// True
+
 // Does "LEARN" contain the subset "RN"?
-//console.log("LEARN".includes ("RN")) True
+console.log("LEARN".includes ("RN")) //True
 
 // Does "LEARN" contain the subset "rn"?
-//console.log("LEARN".includes ("rn"))  FALSE
+console.log("LEARN".includes ("rn")) // FALSE
 
 // Does "LEARN"[0] strictly equal "l"?
-//console.log("LEARN".charAt (0)==="l") FALSE
+console.log("LEARN".charAt (0)==="l") // FALSE
 
 // Modify the code from the previous question to return true.
-//console.log("LEARN"[0]==="L")True
+console.log("LEARN"[0]==="L")//True
 
 //Write a statement that takes a variable of item and logs "in budget" if a price is $100 or less.
 var item = 80
