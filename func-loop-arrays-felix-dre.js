6284f07c7f22d1d7c596fd4804d1d82f55b5288e
FLopez73 <flopez730@gmail.com>
2023-03-20 13:39:31 -0700

    edit

--- func-loop-arrays-felix-dre.js
+++ func-loop-arrays-felix-dre.js
@@ -1,14 +1,7 @@
 // Write a function that takes in an array of numbers and returns an array with all numbers multiplied by 3.
 const testArr1 = [3, 9, 15, 4, 10]
 // output: [9, 27, 45, 12, 30]
-
-const mult = (array) =>{
-    let result = []
-    for (let i = 0; i<array.lenght; i++) {
-        result.join *3 
+function 
+const newNumers () {
+    
 }
-return result
-
-}
-
-console.log(testArr1) 
