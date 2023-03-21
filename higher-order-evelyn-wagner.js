c374692818a476b359e4d3746c85dce64a7bd17a
Ealvarez01 <e.alvarez2404@yahoo.com>
2023-03-21 11:04:28 -0700

    challenge complte

--- higher-order-evelyn-wagner.js
+++ higher-order-evelyn-wagner.js
@@ -1,6 +1,6 @@
 // Write a function that takes in an array of numbers and returns a new array with all numbers multiplied by 10.
 
-const arr1 = [3, 9, 15, 4, 10]
+// const arr1 = [3, 9, 15, 4, 10]
 // // output: [30, 90, 150, 40, 100]
 
 // This is another way to get output:
@@ -8,24 +8,24 @@ const arr1 = [3, 9, 15, 4, 10]
 //     console.log(arr1[index]*10)
 // }
 
-const multiplyByTen = arr1.map((value) => {
-  return value *10
-})
-console.log(multiplyByTen)
+// const multiplyByTen = arr1.map((value) => {
+//   return value *10
+// })
+// console.log(multiplyByTen)
 
 // Write a function that takes in an array of numbers and returns a new array with only odd numbers.
 
-const arr2 = [2, 7, 3, 5, 8, 10, 13]
-// // output: [7, 3, 5, 13]
+// const arr2 = [2, 7, 3, 5, 8, 10, 13]
+// // // output: [7, 3, 5, 13]
 
-const onlyOdd = (array) => {
-  return array.filter((value) => {
-    if(value % 2 !== 0){
-      return "odd"
-    }
-  })
-}
-console.log(onlyOdd(arr2))
+// const onlyOdd = (array) => {
+//   return array.filter((value) => {
+//     if(value % 2 !== 0){
+//       return "odd"
+//     }
+//   })
+// }
+// console.log(onlyOdd(arr2))
 
 // Write a function that takes in an array of numbers and letters and returns a string with only the letters. HINT: Use the typeof operator.
 
@@ -44,21 +44,32 @@ console.log(lettersOnly)
 const filterArrayValues = [58, "", "abcd", true, null, false, 0]
 // // output: [58, "abcd", true]
 
-const arrayWithout = (array) => {
-  let false = array.filter(value) => {
-    return 
-  }
-}
+// Pseudo code:
+// input - an array, of mixed data types
+// output - values that are not false, null, 0 or blank
 
+const arrayWithout = (array) => { 
+  let result = array.filter((value) => {
+    return value !== false && value !==null && value !==0 && value !==""
+  })  
+    return result
+}
+console.log(arrayWithout(filterArrayValues))
 
 // 🏔 Stretch Goals
 
 // Create a function that takes in a string and returns a new string with all the vowels removed.
-// const str = "javascript is awesome"
+
+const str = "javascript is awesome"
 // // output: "jvscrpt s wsm"
 
+const removeVowels = str.replace(/[aeiou]/gi, '') 
+console.log(removeVowels)
 
 // Create a function that takes in two arrays as arguments returns one array with no duplicate values.
-// const arr1 = [3, 7, 10, 5, 4, 3]
-// const arr2 = [7, 8, 2, 1, 5, 4]
+
+const arr1 = [3, 7, 10, 5, 4, 3]
+const arr2 = [7, 8, 2, 1, 5, 4]
 // // output: [3, 7, 10, 5, 4, 8, 2, 1]
+
+console.log(Array.from(new Set(arr1.concat(arr2))))
