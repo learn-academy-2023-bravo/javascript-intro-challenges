9fe62223e5a68cf56c1555f863fd37039b15cb10
MarkNou <mark.nou95@gmail.com>
2023-03-21 09:17:02 -0700

    completed

--- higher-order-mark-eddie.js
+++ higher-order-mark-eddie.js
@@ -11,7 +11,7 @@ const arr1 = [3, 9, 15, 4, 10]
 const multiplyByTen = arr1.map((value) => {
     return value * 10
 })
-// console.log(multiplyByTen) // output: [ 30, 90, 150, 40, 100 ]
+console.log(multiplyByTen) // output: [ 30, 90, 150, 40, 100 ]
 
 // Write a function that takes in an array of numbers and returns a new array with only odd numbers.
 const arr2 = [2, 7, 3, 5, 8, 10, 13]
@@ -26,7 +26,7 @@ const arr2 = [2, 7, 3, 5, 8, 10, 13]
 const onlyOdds = (array) => {
     return array.filter((numbers) => numbers % 2 !== 0)
 }
-// console.log(onlyOdds(arr2)) // output: [ 7, 3, 5, 13 ]
+console.log(onlyOdds(arr2)) // output: [ 7, 3, 5, 13 ]
 
 // Write a function that takes in an array of numbers and letters and returns a string with only the letters. HINT: Use the typeof operator.
 const comboArr = [7, "n", "i", "c", 10, "e", false, "w", 3, "o", "r", "k"]
@@ -43,7 +43,7 @@ const onlyLetters = (array) => {
     })
     return letters.join("")
 }
-// console.log(onlyLetters(comboArr))
+console.log(onlyLetters(comboArr))
 
 // Create a function that takes in an array and returns an array without any false, null, 0, or blank values.
 const filterArrayValues = [58, "", "abcd", true, null, false, 0]
@@ -62,7 +62,7 @@ const selectItems = (array) => {
 
 // 🏔 Stretch Goals
 // Create a function that takes in a string and returns a new string with all the vowels removed.
-// const str = "javascript is awesome"
+const str = "javascript is awesome"
 // // output: "jvscrpt s wsm"
 
 // Create a function noVowels
@@ -71,12 +71,40 @@ const selectItems = (array) => {
 // Filter vowels out with comparative statement
 // Turn array back into string
 
-const noVowels = (array) => {
-let textHere
-}
-
+const noVowels = (string) => {
+    let vowels = ["a", "e", "i", "o", "u"];
+    let textHere = string.split("").filter((value) => !vowels.includes(value))
+    return textHere.join('')
+};
+console.log(noVowels(str))
 
 // Create a function that takes in two arrays as arguments returns one array with no duplicate values.
-// const arr1 = [3, 7, 10, 5, 4, 3]
-// const arr2 = [7, 8, 2, 1, 5, 4]
+const arra1 = [3, 7, 10, 5, 4, 3]
+const arra2 = [7, 8, 2, 1, 5, 4]
 // // output: [3, 7, 10, 5, 4, 8, 2, 1]
+
+//Create function called noDuplicates
+//Concate both arrays into new array held in variable mergedArrays
+//Use HOF filter to return back new array. Only return 
+
+
+const noDuplicates = (array) => {
+    let mergedArrays = array1.concat(array2);
+    let finalArray = mergedArrays.filter((value, index, self) => {
+        return self.indexOf(value) === index
+    })
+
+    return mergedArrays
+}
+
+console.log(noDuplicates)
+
+function mergeArrays(arr1, arr2) {
+     const mergedArray = arr1.concat(arr2);
+    const uniqueArray = mergedArray.filter((value, index, self) => {
+      return self.indexOf(value) === index;
+    });
+    return uniqueArray;
+  }
+const mergedArray = mergeArrays(arra1, arra2);
+console.log(mergedArray); // Output: [1, 2, 3, 4, 5, 6
